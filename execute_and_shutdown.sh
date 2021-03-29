3b0e729af3131ffa3f3ec18ea3af3b76c1a59c48
alexgonca <alexandre.reportagem@gmail.com>
2021-03-29 17:48:43 -0400

    add execute and shutdown

---
+++ execute_and_shutdown.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 main.py
+sudo shutdown -h now
--- init_server.sh
+++ init_server.sh
@@ -2,5 +2,5 @@
 sudo apt update
 sudo apt install -y python3-pip ffmpeg
 pip3 install --trusted-host pypi.python.org -r /home/ubuntu/requirements.txt
-python3 main.py
-sudo shutdown -h now
+sudo chmod +x /home/ubuntu/execute_and_shutdown.sh
+screen -S transcribe /home/ubuntu/execute_and_shutdown.sh
